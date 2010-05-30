9f6a40850016a4cb4af5c843aa4c48c953a35a2c
harlin <dm.harlin@gmail.com>
2010-05-30 23:48:30 +0700

    Basic models

---
+++ skill_tracker/skill_set/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from skill_tracker.skill_set.models import Skill, SubSkill
+
+class SubSkillInline(admin.StackedInline):
+    model = SubSkill
+    extra = 3
+
+class SkillAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General Information', {'fields': ['name', 'description']}),
+        ('Links',               {'fields': ['homepage', 'wiki_en_link'], 'classes': ['collapse']}),
+    ]
+    inlines = [SubSkillInline]
+
+
+admin.site.register(Skill, SkillAdmin)
---
+++ skill_tracker/skill_set/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200)
+    # logo = models.ImageField(upload_to='skill_set/img/')
+    # TODO: learn more about images later. looks like PIL is needed here
+    description = models.TextField()
+    wiki_en_link = models.URLField(verify_exists=True, max_length=200, blank=True)
+    homepage = models.URLField(verify_exists=True, max_length=200, blank=True)
+    def __unicode__(self):
+        return self.name
+
+class SubSkill(models.Model):
+    parent_skill = models.ForeignKey(Skill)
+    name = models.CharField(max_length=200)
+    # logo = models.ImageField(upload_to='skill_set/img/')
+    # TODO: learn more about images later. looks like PIL is needed here
+    description = models.TextField()
+    wiki_en_link = models.URLField(verify_exists=True, max_length=200, blank=True)
+    homepage = models.URLField(verify_exists=True, max_length=200, blank=True)
+    def __unicode__(self):
+        return self.name
+
