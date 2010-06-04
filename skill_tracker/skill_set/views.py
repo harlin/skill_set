b4859120021e7d341bfa6f42668d4826f7eba927
harlin <dm.harlin@gmail.com>
2010-06-05 00:53:47 +0700

    Added Knowledge Base page - and it does display a table, but without real data

--- skill_tracker/skill_set/views.py
+++ skill_tracker/skill_set/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.forms import ModelForm
 from django.forms.models import modelformset_factory, inlineformset_factory
-
+from django import forms
 
 def skill_index(request):
     skill_list = Skill.objects.all()
@@ -61,3 +61,77 @@ def my_skill_input(request, skill_id):
     return render_to_response('skill_set/my_skill_input.html', \
         {'skill': s, 'formset': formset, 'skill_list': skill_list}, \
             context_instance=RequestContext(request))
+
+class SkillSelectorForm(forms.Form):
+    skill_list = [(0, 'Select Skill')]
+    skill_list.extend(
+        [(s.id, s.name) for  s in Skill.objects.all()]
+        )
+    subskill_list = [(0, 'Select Subskill')]
+    subskill_list.extend(
+        [(sub.id, sub.name) for sub in SubSkill.objects.all()]
+        )
+    skill = forms.ChoiceField(choices=skill_list, initial=0, required=False);
+    subskill = forms.ChoiceField(
+        choices=subskill_list, initial=0, required=False);
+
+@login_required
+def knowledge(request):
+    # skill_list = Skill.objects.all()
+    # subskill_list = SubSkill.objects.all()
+    if request.method == 'POST':
+        form = SkillSelectorForm(request.POST)
+        if form.is_valid():
+            sub_list = []
+            if form.cleaned_data['subskill'] != '0':
+                from settings import DEBUG
+                if DEBUG:
+                    somefile = open("test.txt", 'w')
+                    print >> somefile, form.cleaned_data['subskill']
+                    somefile.close()
+                sub_list = [
+                    get_object_or_404(
+                        SubSkill, pk=form.cleaned_data['subskill']
+                        )
+                    ]
+            elif form.cleaned_data['skill'] != '0':
+                s = get_object_or_404(
+                        Skill, pk=form.cleaned_data['skill']
+                        )
+                sub_list = s.subskill_set.all()
+            if not sub_list:
+                return render_to_response('skill_set/knowledge.html', \
+                    {'form': form, 'text': 'Please select skill or subskill'}, \
+                        context_instance=RequestContext(request))
+            user_list = []
+            for sub in sub_list:
+                for k in sub.subskillknowledge_set.all():
+                    if k.knowledge_level != '0' or k.want:
+                        user_list.append(k.employee)
+            user_list = list(set(user_list))
+            data = [['0']]
+            return render_to_response('skill_set/knowledge.html', \
+                {
+                    'form': form, 
+                    'text': 'Please select skill or subskill',
+                    'user_list': user_list,
+                    'sub_list': sub_list,
+                    'data': data }, \
+                    context_instance=RequestContext(request))
+    else:
+        form = SkillSelectorForm()
+        return render_to_response('skill_set/knowledge.html', \
+            {'form': form, 'text': 'Please select skill or subskill'}, \
+                context_instance=RequestContext(request))
+
+# @login_required
+# def knowledge_of_skill(request, skill_id):
+#     s = get_object_or_404(Skill, pk=skill_id)
+#     return render_to_response('skill_set/knowledge_of_skill.html', {'skill': s})
+
+# @login_required
+# def knowledge_of_subskill(request, skill_id, subskill_id):
+#     s = get_object_or_404(Skill, pk=skill_id)
+#     sub = get_object_or_404(SubSkill, pk=subskill_id)
+#     return render_to_response('skill_set/knowledge_of_subskill.html', \
+#         {'skill': s, 'subskill': sub})
