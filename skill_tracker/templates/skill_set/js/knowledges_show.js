9a9c301429dab80efb14eb37861ca88cce8c8e7e
harlin <dm.harlin@gmail.com>
2010-06-06 22:52:43 +0700

    tried to detach JS from html, but failed

---
+++ skill_tracker/templates/skill_set/js/knowledges_show.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $("#skill").change(onSkillChange);
+    $("#subskill").change(onSubSkillChange);
+});
+    function knowledgesJSONTreatment(json)
+    {
+                    var htmlData = "<table>";
+                    htmlData += "<tr>";
+                    htmlData += "<th>User/Skill</th>";
+                    for (i=0; i<json.data.skill_count; i++)
+                    {
+                        htmlData += "<th>";
+                        htmlData += json.data.skills[i].name;
+                        htmlData += "</th>";
+                    }
+                    htmlData += "</tr>";
+                    for (j=0; j<json.data.user_count; j++)
+                    {
+                        htmlData += "<tr>";
+                        htmlData += "<td>";
+                        htmlData += json.data.users[j].name;
+                        htmlData += "</td>";
+                        for (i=0; i<json.data.skill_count; i++)
+                        {
+                            htmlData += "<td>";
+                            htmlData += json.data.users[j].knowledges[i].level;
+                            htmlData += "</td>";
+                        }
+                        htmlData += "</tr>";
+                    }
+                    htmlData += "</table>";
+                    return htmlData;
+    }
+    function onSkillChange(){
+        var selected = $("#skill option:selected");
+        if(selected.val() != 0)
+        {
+            $("#prompt").hide();
+            $.post("/skills/knowledge/", {
+                    skill: selected.val()
+                },
+                function(responseData) {
+                    $("#knowledges").html(knowledgesJSONTreatment(responseData));
+                }
+            );
+        }
+    }
+    function onSubSkillChange(){
+        var selected = $("#subskill option:selected");    
+        if(selected.val() != 0)
+        {
+            $("#prompt").hide();
+            $("#skill").reset();
+            $.post("/skills/knowledge/", {
+                    subskill: selected.val()
+                },
+                function(responseData) {
+                    $("#knowledges").html(knowledgesJSONTreatment(responseData));
+                }
+            );
+        }
+    }
+
